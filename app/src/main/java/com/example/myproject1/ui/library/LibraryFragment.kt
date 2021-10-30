189cb08be8e88cf11bb8e10a859c25f92ab4627c
Ilya-2000 <51691233+Ilya-2000@users.noreply.github.com>
2021-10-30 13:13:08 +0300

    declaring an adapter in a fragment

--- app/src/main/java/com/example/myproject1/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/myproject1/ui/library/LibraryFragment.kt
@@ -1,20 +1,25 @@
 package com.example.myproject1.ui.library
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myproject1.R
+import com.example.myproject1.adapters.LibraryRecyclerAdapter
 import com.example.myproject1.databinding.LibraryFragmentBinding
 
 
-class LibraryFragment : Fragment() {
-    private var _binding: LibraryFragmentBinding? = null
-    private val binding get() = _binding!!
+class LibraryFragment : Fragment(), LibraryRecyclerAdapter.OnItemClickListener {
+    private val TAG = "LibraryFragment"
     private lateinit var navController: NavController
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: LibraryRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +30,21 @@ class LibraryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         navController = findNavController()
-        _binding = LibraryFragmentBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        val binding = LibraryFragmentBinding.inflate(inflater, container, false)
+        recyclerView = binding.projectsLibraryRecyclerview
+        recyclerView.layoutManager = GridLayoutManager(context, 1)
+        adapter = LibraryRecyclerAdapter(this)
+        recyclerView.adapter = adapter
+
+        return binding.root
     }
 
+
+
+    override fun onItemClick(position: Int) {
+        navController.navigate(R.id.action_libraryFragment_to_drawingFragment)
+        Log.d(TAG, "Click for transaction between fragments")
+    }
 
 
 }
