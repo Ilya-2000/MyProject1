6777c19270d60167bea15533078f4c3917557cb4
Ilya-2000 <51691233+Ilya-2000@users.noreply.github.com>
2021-11-02 08:00:59 +0300

    dao ProjectDao.kt add

---
+++ app/src/main/java/com/example/myproject1/data/ProjectDao.kt
@@ -0,0 +1,15 @@
+package com.example.myproject1.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ProjectDao {
+    @Query("SELECT * from project")
+    suspend fun getAll(): List<Project>
+
+    @Insert()
+    suspend fun insertProject(project: Project)
+
+}
